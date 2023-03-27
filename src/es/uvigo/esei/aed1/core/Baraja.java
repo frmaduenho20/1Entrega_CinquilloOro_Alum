77b727fa9d566e11b54c842c4a94b1b51677292b
frmaduenho20 <frmaduenho20@esei.uvigo.es>
2023-03-27 17:10:34 +0200

    ProyectoAEDI

---
+++ src/es/uvigo/esei/aed1/core/Baraja.java
@@ -0,0 +1,54 @@
+/*
+* Representa la baraja española pero con 8 y 9, en total 48 cartas, 4 palos, valores de las cartas de 1 a 12. 
+* Estructura: se utilizará un TAD adecuado
+* Funcionalidad: barajar las cartas, devolver la carta situada encima del montón de cartas
+ */
+
+package es.uvigo.esei.aed1.core;
+
+public class Baraja {
+    public final int CARTAS_BARAJA=48;
+    public final int CARTAS_PALO=12;
+    Carta c;
+    private Carta[] cartas;
+
+    public Baraja(Carta[] cartas) {
+        cartas = new Carta[CARTAS_BARAJA];
+        
+        for (int i = 0; i <= Carta.Palo.values().length; i++) {
+            for (int j = 0; j < CARTAS_PALO; j++) {
+                c = new Carta(Carta.Palo.values()[i], j);
+                cartas[i] = c;  
+            }    
+        }
+    }
+
+    public Carta[] getCartas() {
+        return cartas;
+    }
+
+    public void setCartas(Carta[] cartas) {
+        this.cartas = cartas;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Baraja{");
+        sb.append("\nCARTAS POR BARAJA=").append(CARTAS_BARAJA);
+        sb.append("\nCARTAS POR PALO=").append(CARTAS_PALO);
+        sb.append("\nPALOS: ");
+        for (int i = 0; i < Carta.Palo.values().length; i++) {
+            sb.append(Carta.Palo.values()[i].name());
+        }  
+        sb.append("\nCARTAS");
+        for (int i = 0; i < CARTAS_BARAJA; i++) {
+            sb.append(cartas[i].toString());
+            
+        }
+        sb.append("\n}");
+        return sb.toString();
+    }
+
+    
+}
---
+++ src/es/uvigo/esei/aed1/core/Carta.java
@@ -0,0 +1,45 @@
+/*
+ * Representa una carta, formada por un número y su palo correspondiente
+ */
+
+
+package es.uvigo.esei.aed1.core;
+
+public class Carta {
+    public enum Palo{OROS, ESPADAS, COPAS, BASTOS};
+    private Palo palo; 
+    private int valor;
+
+    public Carta(Palo palo, int valor) {
+        this.palo = palo;
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Palo getPalo() {
+        return palo;
+    }
+
+    public void setPalo(Palo palo) {
+        this.palo = palo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(valor).append(" de ");
+        sb.append(palo);
+        return sb.toString();
+    }
+
+    
+    
+    
+}
