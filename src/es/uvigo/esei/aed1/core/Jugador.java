0201c758b6a54f05037fb2b52345684c8ff481d6
frmaduenho20 <frmaduenho20@esei.uvigo.es>
2023-03-30 09:08:43 +0200

    Borrar

--- src/es/uvigo/esei/aed1/core/Jugador.java
+++ src/es/uvigo/esei/aed1/core/Jugador.java
@@ -3,7 +3,7 @@
  * Estructura mano: se utilizará un TAD adecuado
  * Funcionalidad: Añadir carta a la mano, convertir a String el objeto Jugador (toString)
  */
-//borrar
+//borra
 package es.uvigo.esei.aed1.core;
 import es.uvigo.esei.aed1.iu.IU;
 import es.uvigo.esei.aed1.iu.Main;
