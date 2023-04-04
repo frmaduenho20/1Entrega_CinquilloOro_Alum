f86bef5e1c454956f559bfce1c1001e900dd759c
frmaduenho20 <frmaduenho20@esei.uvigo.es>
2023-04-04 17:40:49 +0200

    Jugador terminada

--- src/es/uvigo/esei/aed1/core/Jugador.java
+++ src/es/uvigo/esei/aed1/core/Jugador.java
@@ -12,5 +12,35 @@ import es.uvigo.esei.aed1.iu.Main;
 
 public class Jugador {
     private final String nombreJugador;
+    private Mano mano;
+
+    public Jugador(String nombreJugador, Mano mano) {
+        this.nombreJugador = nombreJugador;
+        mano = new Mano();
+    }
+
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
+    public Mano getMano() {
+        return mano;
+    }
+
+    public void setMano(Mano mano) {
+        this.mano = mano;
+    }
+    
+    public int getNumCartasMano(){
+        return mano.getNumCartas();
+    }
+    
+    public Carta sacarCartaMano(int pos){
+        return mano.sacarCarta(pos);
+    }
+    
+    public String toStringMano(){
+        return mano.toString();
+    }
     
 }
