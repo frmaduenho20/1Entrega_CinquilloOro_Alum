fe6a97a2bf29841be19da759a11836b312afff97
frmaduenho20 <frmaduenho20@esei.uvigo.es>
2023-04-04 17:11:59 +0200

    Baraja terminada

--- src/es/uvigo/esei/aed1/core/Baraja.java
+++ src/es/uvigo/esei/aed1/core/Baraja.java
@@ -6,56 +6,49 @@
 
 package es.uvigo.esei.aed1.core;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 public class Baraja {
     public final int CARTAS_BARAJA=48;
     public final int CARTAS_PALO=12;
-    Carta c;
-    ArrayList<Carta> cartas = new ArrayList<String>();
-    cartas.add("Volvo");
-    
+    private List<Carta> baraja;
     
     
 //    System.out.println(cartas);
     
-    public Baraja(Carta[] cartas) {
-        cartas = new Carta[CARTAS_BARAJA];
+    public Baraja() {
+
+        baraja = new LinkedList<>();
+        Carta c;
         
         for (int i = 0; i <= Carta.Palo.values().length; i++) {
-            for (int j = 0; j < CARTAS_PALO; j++) {
+            for (int j = 0; j < CARTAS_PALO; j++) {  
                 c = new Carta(Carta.Palo.values()[i], j);
-                cartas[i] = c; 
-                System.out.println(cartas[i]);
+                baraja.add(c);
             }    
         }
-    }
+        
+    } 
+    
+    // Método genérico para aleatorizar una lista en Java utilizando la reproducción aleatoria de Fisher-Yates
+    public static<E> void barajar(List<E> list)
+    {
+        Random random = new Random();
+        int n = list.size();
 
-    public Carta[] getCartas() {
-        return cartas;
-    }
-
-    public void setCartas(Carta[] cartas) {
-        this.cartas = cartas;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Baraja{");
-        sb.append("\nCARTAS POR BARAJA=").append(CARTAS_BARAJA);
-        sb.append("\nCARTAS POR PALO=").append(CARTAS_PALO);
-        sb.append("\nPALOS: ");
-        for (int i = 0; i < Carta.Palo.values().length; i++) {
-            sb.append(Carta.Palo.values()[i].name());
-        }  
-        sb.append("\nCARTAS");
-        for (int i = 0; i < CARTAS_BARAJA; i++) {
-            sb.append(cartas[i].toString());
-            
+        // empieza desde el principio de la lista
+        for (int i = 0; i < n - 1; i++)
+        {
+            // obtener un índice aleatorio `j` tal que `i <= j <= n`
+            int j = i + random.nextInt(n - i);
+ 
+            // intercambiar el elemento en la i-ésima posición en la lista con el elemento en
+            // índice generado aleatoriamente `j`
+            E carta = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, carta);
         }
-        sb.append("\n}");
-        return sb.toString();
     }
-
     
 }
