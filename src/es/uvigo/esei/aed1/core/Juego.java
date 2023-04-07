d5d2cf8233fa0b29137ef52ee481561fd546fd3a
alvaroFerreiros <128727848+alvaroFerreiros@users.noreply.github.com>
2023-04-07 12:10:18 +0200

    Update Juego.java

--- src/es/uvigo/esei/aed1/core/Juego.java
+++ src/es/uvigo/esei/aed1/core/Juego.java
@@ -3,144 +3,32 @@
  * Se recomienda una implementación modular.
  */
 
+//Esto son cambio
+
 package es.uvigo.esei.aed1.core;
+import es.uvigo.esei.aed1.iu.IU;
+import es.uvigo.esei.aed1.iu.Main;
 import cola.*;
 import java.util.Collection;
 
-import cola.Cola;
-//import es.uvigo.esei.aed1.core.Baraja;
-//import es.uvigo.esei.aed1.core.Jugador;
-import es.uvigo.esei.aed1.iu.IU;
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.ListIterator;
-//import java.util.Random;
-
-
 public class Juego{
   private final IU iu;
-  private Cola<Jugador> jugadores;
-  private int numJugadores;
-  private Baraja baraja;
     
     
 public Juego(IU iu){
     this.iu = iu;
-    this.jugadores = new EnlazadaCola();
-    this.numJugadores = pedirNumJugadores();
-    this.baraja = new Baraja();
+
 }
 
-    public void jugar() {
-        System.out.println("Creando Jugadores... ");
-        crearJugadores( iu.pedirDatosJugadores(numJugadores));
-        
-        System.out.println("Barajando...");
-        baraja.barajar();
-        
-        System.out.println("Repartiendo cartas...");
-        repartirCartas(jugadores, baraja);
-        
-        System.out.println("Mostrando jugadores con sus manos...");
-        for (int i = 0; i < numJugadores; i++) {
-            System.out.println(jugadores.primero().toString());
-            jugadores.insertar(jugadores.suprimir());
-        }
-        
-        System.out.println("Empieza el jugador: " 
-                + empiezaJugador(jugadores).getNombreJugador());
-        
-    }
-    public Cola<Jugador> getJugadores() {
-        return jugadores;
-    }
+public void jugar(){
+    int numJugadores = Jugador.class.
+}
 
-    public void setJugadores(Cola<Jugador> jugadores) {
-        this.jugadores = jugadores;
-    }
+public Jugador leeJugador() throws Exception {
+  System.out.println("\nIntroduce los datos del nuevo jugador:");
+  
+  String nombre = leeCadena("\tNombre: ");
 
-    public int getNumJugadores() {
-        return numJugadores;
-    }
-
-    public Baraja getBaraja() {
-        return baraja;
-    }
- 
-    public int pedirNumJugadores(){
-       int numJugadores = 0;
-       
-        do {
-            System.out.println("Cuantos jugadores tendremos en la partida?");
-            numJugadores = iu.leeNum("Selecciona 3 o 4: ");
-        } while (numJugadores != 3 && numJugadores != 4);
-        
-        return numJugadores;
-    }
+  return new Jugador(nombre, mano);
     
-    public void crearJugadores(Collection<String> pedirDatosJugadores){
-        
-        Collection<String> nombresJugadores = pedirDatosJugadores;
-        
-        String [] nombres = new String[numJugadores];
-        nombres = nombresJugadores.toArray(nombres);
-        
-        Jugador j;
-        for (int i = 0; i < nombres.length; i++) {
-            j = new Jugador(nombres[i]);
-            jugadores.insertar(j);
-        }
-        
-    }
-    
-    /**
-     * Reparte las cartas dependiendo de numero de jugadores
-     * si son 3 reparte 16 a cada jugador
-     * si son 4 reparte 12 a cada jugador
-     * 
-     * @param Cola de jugadores
-     * @param Lista baraja ya barajada
-     */
-    public void repartirCartas(Cola<Jugador> jugadores, Baraja baraja){
-        Jugador j;
-        int numCartasMano;
-        
-        if(numJugadores == 3){
-            numCartasMano = 16;
-        }else{
-            numCartasMano = 12;
-        }
-        
-        
-        for (int i = 0; i < jugadores.tamaño(); i++) {
-            j = jugadores.primero();
-            for (int k = 0; k < numCartasMano; k++) {
-                j.getMano().addCarta(baraja.popCarta());
-            }
-            jugadores.suprimir();
-            jugadores.insertar(j);
-        }
-    }
-    /**
-     * Selecciona aleatoriamente el jugador que empieza el jugador
-     * @param Cola de los jugadores
-     * @return jugador que empieza la partida
-     */
-    public Jugador empiezaJugador(Cola<Jugador> jugadores){
-        
-        Jugador j;
-        
-//        Random aleatorio = new Random(System.currentTimeMillis());
-        
-        int posAleatoria;
-        
-        posAleatoria = (int) (Math.random() * numJugadores) + 1;;
-        System.out.println("Posición del jugador que inicia :" + posAleatoria);//Borrar luego
-
-        for (int k = 1; k < posAleatoria; k++){
-            j = jugadores.suprimir();
-            jugadores.insertar(j);
-        }
-        return jugadores.primero();
-    }
 }
