5c12dbd3ae0321f6810d5ec89608489aab767db2
frmaduenho20 <frmaduenho20@esei.uvigo.es>
2023-04-13 09:12:30 +0200

    Inicio de Entrega 2

--- src/es/uvigo/esei/aed1/iu/Main.java
+++ src/es/uvigo/esei/aed1/iu/Main.java
@@ -7,6 +7,7 @@ import es.uvigo.esei.aed1.core.Juego;
 
 //Inicio de Entrega 2 
 
+//Ordenar las manos por orden para la comodidad del juego
 
 public class Main {
     public static void main(String[] args) {
